781279699b7b44a709998308deb8db1a8a8b70a4
Xu, He (Raymond) <raymond.xu@adevinta.com>
2023-03-30 09:38:04 +1100

    Added QR code

--- surls/urls.py
+++ surls/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('manage', views.manage, name='manage'),
     re_path(r'^$', views.launch, name='launch'),
+    path('qr.png', views.qr, name='qr'),
 ]
--- surls/views.py
+++ surls/views.py
@@ -1,16 +1,22 @@
-import ujson
 import requests
 
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse
 from django.views.decorators.cache import cache_page
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Q
 
+import qrcode
+from qrcode.image.pure import PyPNGImage
+from tempfile import SpooledTemporaryFile
+
 from .models import *
 from .forms import *
+
+import logging
+logger = logging.getLogger(__name__)
 
 def index(request):
   return HttpResponse("Hello, world. You're at the polls index.")
@@ -35,10 +41,12 @@ def shorten(request):
             title='N/a',
             keyword=form.cleaned_data['slug']
           )
-          messages.success(request, f"New short URL <a href='http://{domain_name}/{surl.keyword}'> http://{domain_name}/{surl.keyword} </a> created for {test_url}")
+          messages.success(request, f"New short URL <a href='https://{domain_name}/{surl.keyword}'> https://{domain_name}/{surl.keyword} </a> created for {test_url}<br/><img src=\"{surl.keyword}/qr.png\"/>")
           return redirect('/')
-      except Exception:
-        return HttpResponse("Error processing the URL", status=400)
+      except Exception as e:
+        logger.error(e)
+        messages.error(request, "Error processing the URL")
+        return redirect('/')
   return render(request, 'new.html', {'form': form})
 
 def launch(request, keyword):
@@ -68,3 +76,11 @@ def manage(request, keyword):
 
   else:
     return HttpResponse(status=404)
+
+def qr(request, keyword):
+  domain_name = request.META['HTTP_HOST']
+  img = qrcode.make(f'https://{domain_name}/{keyword}', image_factory=PyPNGImage)
+  in_memory_file = SpooledTemporaryFile()
+  img.save(in_memory_file)
+  in_memory_file.seek(0)
+  return FileResponse(in_memory_file, filename='qr.png')
